3a57efe789fbc65713c06ee9c32480932bea8652
Priya Dharshini <priyakanthan2023@gmail.com>
2025-12-23 16:04:16 +0530

    Integrate churn ML model with FastAPI and update gitignore

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,45 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pickle
+import numpy as np
+import os
 
 app = FastAPI(title="Customer Intelligence Platform")
 
 # -----------------------------
-# Input Schema (Request Body)
+# Load trained ML model
 # -----------------------------
-class CustomerInput(BaseModel):
-    age: int
-    monthly_spend: float
-    tenure_months: int
+MODEL_PATH = os.path.join("app", "ml", "model.pkl")
+
+with open(MODEL_PATH, "rb") as f:
+    model = pickle.load(f)
+
+# -----------------------------
+# Input Schema (MUST MATCH TRAINING FEATURES)
+# -----------------------------
+class CustomerData(BaseModel):
+    account_length: int
+    international_plan: int   # 0 = No, 1 = Yes
+    voice_mail_plan: int      # 0 = No, 1 = Yes
+    number_vmail_messages: int
+
+    total_day_minutes: float
+    total_day_calls: int
+    total_day_charge: float
+
+    total_eve_minutes: float
+    total_eve_calls: int
+    total_eve_charge: float
+
+    total_night_minutes: float
+    total_night_calls: int
+    total_night_charge: float
+
+    total_intl_minutes: float
+    total_intl_calls: int
+    total_intl_charge: float
+
+    customer_service_calls: int
 
 # -----------------------------
 # Health Check
@@ -19,17 +49,38 @@ def health_check():
     return {"status": "API running successfully"}
 
 # -----------------------------
-# Prediction Endpoint (Mock)
+# Churn Prediction Endpoint
 # -----------------------------
 @app.post("/predict")
-def predict_customer_behavior(data: CustomerInput):
-    # Dummy logic (we’ll replace with ML model)
-    score = (data.monthly_spend * 0.6) + (data.tenure_months * 0.4)
+def predict_churn(data: CustomerData):
 
-    risk = "High" if score < 500 else "Low"
+    features = np.array([[  
+        data.account_length,
+        data.international_plan,
+        data.voice_mail_plan,
+        data.number_vmail_messages,
+
+        data.total_day_minutes,
+        data.total_day_calls,
+        data.total_day_charge,
+
+        data.total_eve_minutes,
+        data.total_eve_calls,
+        data.total_eve_charge,
+
+        data.total_night_minutes,
+        data.total_night_calls,
+        data.total_night_charge,
+
+        data.total_intl_minutes,
+        data.total_intl_calls,
+        data.total_intl_charge,
+
+        data.customer_service_calls
+    ]])
+
+    prediction = model.predict(features)[0]
 
     return {
-        "customer_score": score,
-        "risk_level": risk
+        "churn_prediction": "Yes" if prediction == 1 else "No"
     }
-
